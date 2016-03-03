9b58d2fc2cf309563224205fa078ddf95d054c40
Vincent Lesierse <vincent.lesierse@exact.com>
2016-03-03 21:09:32 +0100

    Added support for dotnet test

---
+++ src/Cake.Common.Tests/Fixtures/Tools/DotNetCore/Test/DotNetCoreTesterFixture.cs
@@ -0,0 +1,15 @@
+﻿using Cake.Common.Tools.DotNetCore.Test;
+
+namespace Cake.Common.Tests.Fixtures.Tools.DotNetCore.Test
+{
+    internal sealed class DotNetCoreTesterFixture : DotNetCoreFixture<DotNetCoreTestSettings>
+    {
+        public string Path { get; set; }
+
+        protected override void RunTool()
+        {
+            var tool = new DotNetCoreTester(FileSystem, Environment, ProcessRunner, Globber);
+            tool.Test(Path, Settings);
+        }
+    }
+}
---
+++ src/Cake.Common/Tools/DotNetCore/Test/DotNetCoreTestSettings.cs
@@ -0,0 +1,20 @@
+﻿using Cake.Core.IO;
+
+namespace Cake.Common.Tools.DotNetCore.Test
+{
+    /// <summary>
+    /// Contains settings used by <see cref="DotNetCoreTester" />.
+    /// </summary>
+    public class DotNetCoreTestSettings : DotNetCoreSettings
+    {
+        /// <summary>
+        /// Gets or sets the configuration under which to build.
+        /// </summary>
+        public string Configuration { get; set; }
+
+        /// <summary>
+        /// Gets or sets the output directory.
+        /// </summary>
+        public DirectoryPath OutputDirectory { get; set; }
+    }
+}
